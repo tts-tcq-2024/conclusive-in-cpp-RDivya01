db7769d12b63142b8d89e7df5f20cc239dafe53b
Divya Rani <91890152+RDivya01@users.noreply.github.com>
2024-08-16 11:37:14 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -11,24 +11,26 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
+void getCoolingLimits(CoolingType coolingType, int* lowerLimit, int* upperLimit) {
   switch(coolingType) {
     case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
+      *lowerLimit = 0;
+      *upperLimit = 35;
       break;
     case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
+      *lowerLimit = 0;
+      *upperLimit = 45;
       break;
     case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
+      *lowerLimit = 0;
+      *upperLimit = 40;
       break;
   }
+}
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+  int lowerLimit = 0, upperLimit = 0;
+  getCoolingLimits(coolingType, &lowerLimit, &upperLimit);
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
@@ -54,18 +56,16 @@ void sendToController(BreachType breachType) {
   printf("%x : %x\n", header, breachType);
 }
 
+void sendLowOrHighEmail(BreachType breachType, const char* recepient) {
+  const char* message = breachType == TOO_LOW ? 
+                        "Hi, the temperature is too low\n" : 
+                        "Hi, the temperature is too high\n";
+  printf("To: %s\n%s", recepient, message);
+}
+
 void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
+  if (breachType != NORMAL) {
+    sendLowOrHighEmail(breachType, recepient);
   }
 }
